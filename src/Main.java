43d7c626a58dc88ae9d90fa0eab4edf1faca4bad
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-04 10:27:08 +0800

    继承JPanel初始化窗口

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Main {
+public class Main extends JPanel{
+    public Main(){
+        setSize(new Dimension(800,480));
+        setPreferredSize(new Dimension(800,480));
+        setBackground(Color.BLACK);
+    }
 
     public static void main(String[] args) {
+        Main game = new Main();
         JFrame jFrame = new JFrame();
         jFrame.setTitle("Initialize the rectangles");
-        jFrame.setSize(new Dimension(800,480));
+        jFrame.add(game);
+        jFrame.pack();
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
