4c1bbddfa85e930f9a3dd00819740f26c071282a
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-04 10:13:59 +0800

    初始化一个窗口

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame jFrame = new JFrame();
+        jFrame.setTitle("Initialize the rectangles");
+        jFrame.setSize(new Dimension(800,480));
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
     }
 }
