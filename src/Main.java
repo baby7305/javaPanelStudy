0863e36ad8c82c1365b8d9e6570ff26716e2cba7
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-04 11:21:56 +0800

    键盘控制椭圆移动

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Main extends JPanel implements KeyListener{
+public class Main extends JPanel implements KeyListener {
 	private int x = 5;
 	private int y = 5;
 
@@ -11,6 +11,8 @@ public class Main extends JPanel implements KeyListener{
 		setSize(new Dimension(800, 480));
 		setPreferredSize(new Dimension(800, 480));
 		setBackground(Color.BLACK);
+		setFocusable(true);
+		addKeyListener(this);
 	}
 
 	public void paint(Graphics graphics) {
@@ -23,6 +25,20 @@ public class Main extends JPanel implements KeyListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+		int c = e.getKeyCode();
+		if (c == KeyEvent.VK_W) {
+			y--;
+		}
+		if (c == KeyEvent.VK_S) {
+			y++;
+		}
+		if (c == KeyEvent.VK_A) {
+			x--;
+		}
+		if (c == KeyEvent.VK_D) {
+			x++;
+		}
+
 
 	}
 
