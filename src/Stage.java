2d3f27cde48db7ed7ed58e6805de0d9889076f6f
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-04 18:54:36 +0800

    调整stage对象

--- src/Stage.java
+++ src/Stage.java
@@ -4,15 +4,14 @@ import java.awt.*;
  * Created by Administrator on 2018/1/4.
  */
 public class Stage {
-	private Rectangle platform = new Rectangle(0, 540, 800, 20);
+	private Rectangle platform;
 
 	public Stage() {
-
+		platform = new Rectangle(0, 540, 800, 20);
 	}
 
 	public void draw(Graphics graphics) {
 		graphics.setColor(Color.WHITE);
-		graphics.fillRect(0, 540, 800, 20);
-//		graphics.fillRect(platform.x, platform.y, platform.width, platform.height);
+		graphics.fillRect(platform.x, platform.y, platform.width, platform.height);
 	}
 }
