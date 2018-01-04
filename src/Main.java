5352e324f5b4d135740bc5544183f51716766d6f
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-04 10:29:44 +0800

    不让窗口缩放，格式化代码

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Main extends JPanel{
-    public Main(){
-        setSize(new Dimension(800,480));
-        setPreferredSize(new Dimension(800,480));
-        setBackground(Color.BLACK);
-    }
+public class Main extends JPanel {
+	public Main() {
+		setSize(new Dimension(800, 480));
+		setPreferredSize(new Dimension(800, 480));
+		setBackground(Color.BLACK);
+	}
 
-    public static void main(String[] args) {
-        Main game = new Main();
-        JFrame jFrame = new JFrame();
-        jFrame.setTitle("Initialize the rectangles");
-        jFrame.add(game);
-        jFrame.pack();
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setVisible(true);
-    }
+	public static void main(String[] args) {
+		Main game = new Main();
+		JFrame jFrame = new JFrame();
+		jFrame.setTitle("Initialize the rectangles");
+		jFrame.add(game);
+		jFrame.pack();
+		jFrame.setResizable(false);
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.setLocationRelativeTo(null);
+		jFrame.setVisible(true);
+	}
 }
